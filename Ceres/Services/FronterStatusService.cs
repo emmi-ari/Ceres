0c794835ee07ff001ccecf553718c06f2f1eae0c
Emmi Ari Ünver <emmiariunver@gmail.com>
2023-02-27 13:22:41 +0100

    Custom Front - change workplace

--- Ceres/Services/FronterStatusService.cs
+++ Ceres/Services/FronterStatusService.cs
@@ -1,4 +1,5 @@
-﻿using Ceres.Models;
+﻿using System.Diagnostics;
+using Ceres.Models;
 using Ceres.Models.Apparyllis;
 
 using Discord;
@@ -21,7 +22,7 @@ namespace Ceres.Services
         public FronterStatusService(DiscordSocketClient discord, IConfigurationRoot config)
         {
             _commonFronterStatus = new(discord, config);
-#if RELEASE
+#if !RELEASE
             _timer = new PeriodicTimer(TimeSpan.FromMinutes(10));
             TriggerStatusRefresh();
 #endif
@@ -42,7 +43,8 @@ namespace Ceres.Services
         private readonly DiscordSocketClient _discord;
         private readonly IConfigurationRoot _config;
         private readonly LoggingService _logger;
-        private readonly Dictionary<string, string> _memberIdNames;
+        private readonly Dictionary<string, string> _memberIdRelation;
+        private readonly Dictionary<string, string> _customFrontIdRelation;
         private readonly HttpClient _request;
 
         internal CommonFronterStatusMethods(DiscordSocketClient discord, IConfigurationRoot config)
@@ -50,7 +52,10 @@ namespace Ceres.Services
             _discord = discord;
             _config = config;
             _logger = new();
-            _memberIdNames = GetMemberIdNames();
+            Debug.WriteLine("=============== DEBUG ===============");
+            _memberIdRelation = GetMemberIdNames();
+            _customFrontIdRelation = GetCustomFrontIdRelation();
+            // Debug.WriteLine(_customFrontIdRelation[]);
             HttpClient request = new()
             {
                 BaseAddress = new("https://api.apparyllis.com:8443")
@@ -58,6 +63,19 @@ namespace Ceres.Services
             request.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", _config["apparyllis.simplyplural_token"]);
             request.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
             _request = request;
+        }
+
+        private Dictionary<string, string> GetCustomFrontIdRelation()
+        {
+            string[] memberIdArray = _config["apparyllis.customFronts"].Split(' ');
+            Dictionary<string, string> memberIdNames = new(memberIdArray.Length);
+            foreach (string memberId in memberIdArray)
+            {
+                string[] memberIdAux = memberId.Split('_');
+                memberIdNames.Add(memberIdAux[1].Trim(), memberIdAux[0].Trim());
+                Debug.WriteLine($"Key {memberIdAux[1]}\nValue{memberIdAux[0]}");
+            }
+            return memberIdNames;
         }
 
         private Dictionary<string, string> GetMemberIdNames()
@@ -74,29 +92,21 @@ namespace Ceres.Services
 
         internal async Task<string> SetFronterStatusAsync()
         {
-#if DEBUG
+#if !DEBUG
             string statusMessage = "DBG - No fronter info";
             await _logger.OnLogAsync(new(LogSeverity.Debug, nameof(this.SetFronterStatusAsync), $"Debug"));
 #else
-            List<FrontMemberInfos> serializedFronterList = await GetFrontersList();
-            string statusMessage = string.Empty;
+            List<FrontMemberInfos>[] frontInfos = await GetFrontersList();
+            var serializedFronterList = frontInfos[0];
+            var serializedCustomFrontList = frontInfos[1];
+            string statusMessage = "Front: ";
 
-            try
+            serializedFronterList.ForEach(member => statusMessage += $"{member.MemberName}, ");
+            if (serializedCustomFrontList.Count > 0)
             {
-                statusMessage = serializedFronterList.Count switch
-                {
-                    0 => "No fronter info for now",
-                    1 => $"{serializedFronterList[0].MemberName} is fronting",
-                    2 => $"{serializedFronterList[0].MemberName} and {serializedFronterList[1].MemberName} are fronting",
-                    3 => $"{serializedFronterList[0].MemberName}, {serializedFronterList[1].MemberName} and {serializedFronterList[2].MemberName} are fronting",
-                    _ => throw new ArgumentException($"Unusual amount ({serializedFronterList?.Count}) of fronters in response", nameof(serializedFronterList))
-                };
-            }
-            catch (ArgumentException ex)
-            {
-                statusMessage = "StatusGeneratorException (lol)";
-                await _logger.OnLogAsync(new(LogSeverity.Error, nameof(this.SetFronterStatusAsync), $"Unusual amount of fronters recieved ({serializedFronterList.Count})", ex));
-                throw ex;
+                statusMessage = $"{statusMessage.TrimEnd(',').TrimEnd(' ')} (";
+                serializedCustomFrontList.ForEach(member => statusMessage += $"{member.MemberName}, ");
+                statusMessage = $"{statusMessage.TrimEnd(',').TrimEnd(' ')})";
             }
 #endif
 
@@ -107,7 +117,7 @@ namespace Ceres.Services
 
         private async Task<ApparyllisModel> GetFrontStatusAsync()
         {
-#if RELEASE
+#if !RELEASE
             
             HttpResponseMessage frontingStatusResponse = await _request.GetAsync("/v1/fronters/");
             string response = await frontingStatusResponse.Content.ReadAsStringAsync();
@@ -118,7 +128,7 @@ namespace Ceres.Services
 
             ApparyllisModel serializedResponse = JsonConvert.DeserializeObject<ApparyllisModel>(responseSingularFronter);
 #endif
-#if RELEASE
+#if !RELEASE
             ApparyllisModel serializedResponse = JsonConvert.DeserializeObject<ApparyllisModel>(response);
             string logMessage = $"Recieved response from apparyllis server: {frontingStatusResponse.StatusCode}";
             LogSeverity severity = LogSeverity.Info;
@@ -133,18 +143,19 @@ namespace Ceres.Services
             return serializedResponse;
         }
 
-        private async Task<List<FrontMemberInfos>> GetFrontersList()
+        private async Task<List<FrontMemberInfos>[]> GetFrontersList()
         {
             ApparyllisModel response = await GetFrontStatusAsync();
             return ParseMembers(response);
         }
 
-        private List<FrontMemberInfos> ParseMembers(ApparyllisModel responseSerialized)
+        private List<FrontMemberInfos>[] ParseMembers(ApparyllisModel responseSerialized)
         {
             if (responseSerialized == null) throw new ArgumentNullException(paramName: nameof(responseSerialized), string.Empty);
 
             int fronterCount = responseSerialized.Response.Length;
             List<FrontMemberInfos> serializedFronterList = new(fronterCount);
+            List<FrontMemberInfos> serializedCustomFrontList = new(fronterCount);
 
             for (int i = 0; i < fronterCount; i++)
             {
@@ -153,17 +164,25 @@ namespace Ceres.Services
                     continue;
                     try
                     {
-                        var test = _memberIdNames[fronter.Member];
+                        var test = _memberIdRelation[fronter.Member];
                     }
                     catch (KeyNotFoundException)
                     {
-                        continue;
+                        try
+                        {
+                            var test = _customFrontIdRelation[fronter.Member];
+                        }
+                        catch (KeyNotFoundException) { continue; }
+
+                        serializedCustomFrontList.Add(new(_customFrontIdRelation[fronter.Member], fronter.StartTime, fronter.EndTime));
                     }
 
-                serializedFronterList.Add(new (_memberIdNames[fronter.Member], fronter.StartTime, fronter.EndTime));
+                serializedFronterList.Add(new (_memberIdRelation[fronter.Member], fronter.StartTime, fronter.EndTime));
             }
 
-            return serializedFronterList;
+            List<FrontMemberInfos>[] retArray = {serializedFronterList, serializedCustomFrontList };
+
+            return retArray;
         }
     }
 }
