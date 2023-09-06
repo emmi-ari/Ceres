81cc8075370ca67ae071a95cf2ef2626347e6946
Emmi Ari <28660694+Nihilopia@users.noreply.github.com>
2023-09-06 15:07:48 +0200

    Cleaned up Program.cs

--- CeresDSP/Program.cs
+++ CeresDSP/Program.cs
@@ -1,27 +1,12 @@
-﻿using DSharpPlus;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace CeresDSP
 {
     internal class Program
     {
         private static Task Main() => new Program().MainAsync();
-        //static async Task Main()
-        //{
-        //    DiscordClient discord = new(new DiscordConfiguration()
-        //    {
-        //        Token = await File.ReadAllTextAsync(@"C:\Users\emmia\token"),
-        //        TokenType = TokenType.Bot,
-        //        Intents = (DiscordIntents)0x1FFFF
-        //    });
-        //    await discord.ConnectAsync();
-        //    discord.MessageCreated += async (s, e) =>
-        //    {
-        //        if (e.Message.Content.ToLower().StartsWith("ping"))
-        //            await e.Message.RespondAsync("pong!");
-        //    };
-        //    await Task.Delay(-1);
-        //}
-    
+
+        [SuppressMessage("Performance", "CA1822:Mark members as static")]
         private async Task MainAsync()
         {
             Ceres ceres = new();
