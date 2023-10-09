f0c2f20f241d274b262f6ebc1b54caa577adf1dd
Emmi Ari <28660694+Nihilopia@users.noreply.github.com>
2023-10-09 13:42:21 +0200

    Deleted obsolete project/bot

--- Ceres/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0018:Inline variable declaration", Scope = "member", Target = "~M:Ceres.Services.CommandHandler.CommandsCollection.AddReaction(System.String,System.String)~System.Threading.Tasks.Task")]
