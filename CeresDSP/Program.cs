0b267b86e66d1fb9975dba9f174e4918cf1c2701
Ari <emmiunver@gmail.com>
2025-06-25 23:27:12 +0200

    Exception handeling, for when bot client authentication fails.

--- CeresDSP/Program.cs
+++ CeresDSP/Program.cs
@@ -5,7 +5,18 @@
         private static async Task Main()
         {
             Ceres ceres = new();
-            await ceres.ConnectAsync();
+            try
+            {
+                await ceres.ConnectAsync();
+            }
+            catch (Exception ex)
+            {
+                ConsoleColor defaultColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{ex.Message} (0x{ex.HResult:x8})");
+                Console.ForegroundColor = defaultColor;
+                return;
+            }
             await Task.Delay(-1);
         }
     }
